6fefaca9d0ae47b09dffca4a4fe8954f4e374eaa
Ivan Zheng <zivan888@gmail.com>
2022-09-01 01:11:23 +0000

    test.py new test

--- src/mywork/test.py
+++ src/mywork/test.py
@@ -1,6 +1,21 @@
 from z3 import *
+# template, set varaibles, add formulas, add everything to a solver, check the  solver's list, assign the variables using models
 x = Bool('x')
 y = Bool('y')
 z = Bool('z')
 
+f1 = Or(x,y,z)
+f2 = Or(Not(x), Not(y))
+f3 = Or(Not(y), Not(z))
 
+F = And(f1, f2, f3)
+
+s = Solver()
+s.add(F)
+print(s.check()) # Checks whats in the solver
+
+m = s.model()
+
+print(m.evaluate(x))
+print(m.evaluate(y))
+print(m.evaluate(z))
